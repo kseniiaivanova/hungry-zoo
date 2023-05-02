c07ce7b1573b4c51389d2a4a23725334f529710d
Ksenia <ksenia.ivanovva@gmail.com>
2023-05-02 09:56:01 +0200

    feat: created pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { Home } from "./pages/Home";
+import { AnimalList } from "./pages/AnimalList";
+import { AnimalView } from "./pages/AnimalView";
+import { Error } from "./pages/Error";
+import { RouterProvider } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <Home></Home>,
+    errorElement: <Error></Error>
+
+  },
+
+  {
+    path: "/animallist",
+    element: <AnimalList></AnimalList>,
+
+  },
+
+
+  {
+    path: "/albumlist/:id",
+    element: <AnimalView></AnimalView>
+
+  },
+
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
---
+++ src/pages/Error.tsx
@@ -0,0 +1,4 @@
+export const Error = () => {
+    return (<>
+        <h1>Sorry, nothing is found here, try again</h1></>)
+}; 
