4897bf353070f0635ef01bcbcb0a19ee3df1c997
Ksenia <ksenia.ivanovva@gmail.com>
2023-05-02 11:06:36 +0200

    feat: got data from API

---
+++ src/components/Animal.tsx
@@ -0,0 +1,13 @@
+export interface IAnimal {
+    id: number,
+    name: string,
+    latinName: string,
+    yearOfBirth: string,
+    shortDescription: string,
+    longDescription: string,
+    imageUrl: string,
+    medicine: string,
+    isFed: false,
+    lastFed: string
+
+}; 
