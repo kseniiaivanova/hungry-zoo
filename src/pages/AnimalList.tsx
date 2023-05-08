df55d35bc04451b1872a75388ade9aa0d56b4862
Ksenia <ksenia.ivanovva@gmail.com>
2023-05-08 15:45:00 +0200

    feat: added mixins

--- src/pages/AnimalList.tsx
+++ src/pages/AnimalList.tsx
@@ -1,41 +1,41 @@
 import axios from "axios";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { IAnimal } from "../components/Animal";
 import { Navbar } from "../components/Navbar";
 import { Link } from "react-router-dom";
 import { Animal } from "../components/Animal";
 
 export const AnimalList = () => {
-    const [animals, setAnimals] = useState<IAnimal[]>(JSON.parse(localStorage.getItem('animals') || '[]'));
+  const [animals, setAnimals] = useState<IAnimal[]>(
+    JSON.parse(localStorage.getItem("animals") || "[]")
+  );
 
-    useEffect(() => {
-        if (animals.length === 0) {
-            axios.get<IAnimal[]>("https://animals.azurewebsites.net/api/animals").then(response => {
-                setAnimals(response.data);
-                localStorage.setItem('animals', JSON.stringify(response.data));
-            }).catch(error => console.log(error));
+  useEffect(() => {
+    if (animals.length === 0) {
+      axios
+        .get<IAnimal[]>("https://animals.azurewebsites.net/api/animals")
+        .then((response) => {
+          setAnimals(response.data);
+          localStorage.setItem("animals", JSON.stringify(response.data));
+        })
+        .catch((error) => console.log(error));
+    }
+  }, []);
 
+  return (
+    <> <div className="listWrapper">
+      <Navbar></Navbar>
 
-        }
-    }, []);
-
-
-
-    return (
-        <>
-            <Navbar></Navbar>
-
-            {animals.map((animal) => (
-                <>
-                    <Link key={animal.id} to={animal.id.toString()}>
-                        <Animal {...animal}></Animal>
-                        <p>Läs mer</p>
-
-                    </Link>
-
-
-                </>
-            ))}
+      {animals.map((animal) => (
+        <> 
+          <Link key={animal.id} to={animal.id.toString()}>
+            <Animal {...animal}></Animal>
+            <p className="more">Läs mer</p>
+              </Link>
+              
         </>
-    );
-}
+      ))}
+          </div>
+    </>
+  );
+};
